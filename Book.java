e02d5f587e43b79a8c9d3a19131d469fb013e42c
Miraj <shahriarmiraj321@gmail.com>
2025-10-25 19:44:45 +0600

    Add BOOK class abd constructors

--- Book.java
+++ Book.java
@@ -1,39 +1,40 @@
 
-public class Book {
+public class BOOK {
+  
     private String title;
     private String author;
     private double price;
 
-    public Book() {
-        this.title = "x";
-        this.author = "y";
-        this.price = 400.00 ;
+    public BOOK() {
+        
+        this.title = "A";
+        this.author = "B";
+        this.price = 350.00;
     }
 
-    public Book(String title, String author) {
+    public BOOK(String title, String author) {
         this.title = title;
         this.author = author;
-        this.price = 500.00;
+        this.price = 300.00;
     }
 
-    public Book(String title, String author, double price) {
-        
+    public BOOK(String title, String author, double price) {
         this.title = title;
         this.author = author;
         this.price = price;
     }
     public static void main(String[] args) {
-        Book book1 = new Book();
+        BOOK book1 = new BOOK();
         System.out.println("Book1 Title: " + book1.title);
         System.out.println("Book1 Author: " + book1.author);
         System.out.println("Book1 Price: " + book1.price);
 
-        Book book2 = new Book("A", "B");
+        BOOK book2 = new BOOK("C", "D");
         System.out.println("Book2 Title: " + book2.title);
         System.out.println("Book2 Author: " + book2.author);
         System.out.println("Book2 Price: " + book2.price);
 
-        Book book3 = new Book("C", "D", 450.99);
+        BOOK book3 = new BOOK("X", "Y", 400.00);
         System.out.println("Book3 Title: " + book3.title);
         System.out.println("Book3 Author: " + book3.author);
         System.out.println("Book3 Price: " + book3.price);
